036b0856964de3d4a5301e5e8c3ef8a687fed68e
y-shi23 <y-shi23tsinghua@outlook.com>
2026-03-25 22:22:22 +0800

    chore: bump version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.1.1"
         manifestPlaceholders["operitDocumentsAuthority"] = "${applicationId}.documents.ubuntu"
 
         ndk {
